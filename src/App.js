f9927181ee774e2b02fe75669af0b818faa66e8d
Kevin Liang <kevinliang0303@gmail.com>
2022-11-14 08:19:04 -0800

    Initialize routes for Dashboard, Signup, and Login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
 import "./App.css";
 import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <AuthProvider>
-      <Signup />;
-    </AuthProvider>
+    <>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </>
   );
 }
 
